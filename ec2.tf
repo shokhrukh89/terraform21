13661d5333827b52e117df6eee261467fa872957
shokhrukh89 <88409779+shokhrukh89@users.noreply.github.com>
2021-09-11 10:29:46 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "sensitive" {
     ami = "ami-087c17d1fe0178315"
-    region = "us-east-1"
     instance_type = "t2.micro"
 
     tags = {
-        Name = "sensitive"
+        Name = "terraform-cloud"
     }
 }
